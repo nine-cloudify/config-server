0ee699afc798c10da6e973f25f7b364985aad582
Jarvis <wangjia_1919@163.com>
2018-05-15 17:04:56 +0800

    修改父ID 增加 注册到注册中心

--- src/main/java/com/springcloudify/cloudy/configserver/ConfigServerApplication.java
+++ src/main/java/com/springcloudify/cloudy/configserver/ConfigServerApplication.java
@@ -1,7 +1,6 @@
 package com.springcloudify.cloudy.configserver;
 
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -10,6 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigServerApplication.class).web(WebApplicationType.SERVLET).run(args);
+        SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
