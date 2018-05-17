e29ee08304ea3a94d39161218b29d9847be281c7
Jarvis <wangjia_1919@163.com>
2018-05-17 17:15:32 +0800

    修改高可用 jdbc backend

--- src/main/java/com/springcloudify/cloudy/configserver/ConfigServerApplication.java
+++ src/main/java/com/springcloudify/cloudy/configserver/ConfigServerApplication.java
@@ -1,11 +1,13 @@
 package com.springcloudify.cloudy.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringCloudApplication
 @EnableConfigServer
+@EntityScan(basePackages = "com.**.entity")
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
