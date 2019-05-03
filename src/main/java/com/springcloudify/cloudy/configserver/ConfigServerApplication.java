06d6ad1b7b504fa9b560d239efb4c807e2b03d3f
王佳 <wangjia_1919@163.com>
2019-05-03 12:56:04 +0800

    update config file error

--- src/main/java/com/springcloudify/cloudy/configserver/ConfigServerApplication.java
+++ src/main/java/com/springcloudify/cloudy/configserver/ConfigServerApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringCloudApplication
-@EnableConfigServer
-@EntityScan(basePackages = "com.**.entity")
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/springcloudify/cloudy/configserver/ConfigServerConfig.java
@@ -0,0 +1,16 @@
+package com.springcloudify.cloudy.configserver;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Create by Jarvis.wang on Jarvis's device
+ *
+ * @author Jarvis.wang  Jarvis
+ * @date 2019-05-07 12:37
+ */
+@Configuration
+@EnableConfigServer
+//@EntityScan(basePackages = "com.**.entity")
+public class ConfigServerConfig {
+}
