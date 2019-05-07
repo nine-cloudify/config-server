f5b374cf143aed842e1a902d686f2d8c90b1c3ce
王佳 <wangjia_1919@163.com>
2019-05-07 17:43:53 +0800

    update config

--- src/main/java/com/springcloudify/cloudify/configserver/repository/PropRepository.java
+++ src/main/java/com/springcloudify/cloudify/configserver/repository/PropRepository.java
@@ -1,7 +1,6 @@
-<<<<<<< HEAD:src/main/java/com/springcloudify/cloudy/configserver/repository/PropRepository.java
-//package com.springcloudify.cloudy.configserver.repository;
+//package com.springcloudify.cloudify.configserver.repository;
 //
-//import com.springcloudify.cloudy.configserver.entity.Properties;
+//import com.springcloudify.cloudify.configserver.entity.Properties;
 //import org.springframework.data.repository.CrudRepository;
 //
 ///**
@@ -14,20 +13,3 @@
 //public interface PropRepository extends CrudRepository<Properties, Long> {
 //
 //}
-=======
-package com.springcloudify.cloudify.configserver.repository;
-
-import com.springcloudify.cloudify.configserver.entity.Properties;
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created with IntelliJ IDEA.
- * User: Erasme
- * Date: 2018/5/17
- * Time: 00:22
- * Description:
- */
-public interface PropRepository extends CrudRepository<Properties, Long> {
-
-}
->>>>>>> 5a637ee84ecac0ef54ffb695a531c15ff3130f7a:src/main/java/com/springcloudify/cloudify/configserver/repository/PropRepository.java
