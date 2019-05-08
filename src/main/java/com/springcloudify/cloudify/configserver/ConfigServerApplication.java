3d441c6dd7916b235fdd9da4f85863dae71caa6a
王佳 <wangjia_1919@163.com>
2019-05-08 17:38:49 +0800

    update all

--- src/main/java/com/springcloudify/cloudify/configserver/ConfigServerApplication.java
+++ src/main/java/com/springcloudify/cloudify/configserver/ConfigServerApplication.java
@@ -1,9 +1,7 @@
 package com.springcloudify.cloudify.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringCloudApplication
 public class ConfigServerApplication {
